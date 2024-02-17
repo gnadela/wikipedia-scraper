a39c5c4519e722a3832d06b32c1367d7cce780c5
Geraldine <gnadela@gmail.com>
2024-02-17 16:22:38 +0100

    Update README

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-import json
 from src.scraper import WikipediaScraper
-from src.utils import read_json, write_json
+from src.utils import write_json
 
 scraper = WikipediaScraper()
-countries = scraper.get_countries() # Loop through each country and query the /leaders endpoint
-print(countries)
+countries = scraper.get_countries() # Get the list of countries
+
+# Retrieve leaders data for each country
 for country in countries: 
     scraper.get_leaders(country) 
 
-leaders_per_country = scraper.leaders_data
-        
+# Get Wikipedia URLs of leaders        
 wikipedia_urls = []
-for country, leaders in leaders_per_country.items():
-    # Iterate over each leader and get their wikipedia_url
+for leaders in scraper.leaders_data.values():
     for leader in leaders:
-        url = leader['wikipedia_url']
-        wikipedia_urls.append(url)
+        url = leader.get('wikipedia_url')
+        if url:
+            wikipedia_urls.append(url)
 
 leader_paragraphs = []
 for url in wikipedia_urls:
+    # Retrieve the first paragraph for each leader's Wikipedia page
     first_paragraph = WikipediaScraper.get_first_paragraph(url)
-    cleaned_paragraph = first_paragraph.text
-    print(url)
-    print(cleaned_paragraph)
-    leader_paragraphs.append(cleaned_paragraph)
-
+    if first_paragraph:
+        cleaned_paragraph = first_paragraph.text.strip() # remove trailing or leading white spaces
+        print(url)
+        print(cleaned_paragraph)
+        leader_paragraphs.append({'url': url, 'first_paragraph': cleaned_paragraph})
+    else:
+        print((f"Unable to retrieve first paragraph for {url}"))
+        
 write_json(leader_paragraphs, 'leader_briefs.json')
--- src/scraper.py
+++ src/scraper.py
@@ -11,26 +11,31 @@ class WikipediaScraper:
         self.leaders_data = {}
         self.cookie = self.refresh_cookie()
 
-    def refresh_cookie(self):
+    def refresh_cookie(self) -> str:
+        '''Refreshes the API cookie.'''
         cookie_url = self.base_url + self.cookies_endpoint
         cookie_response = requests.get(cookie_url)
         cookie = cookie_response.cookies.get('user_cookie')
         return cookie
    
     def get_countries(self):
+        '''Retrieves the list of countries.'''
         countries_url = self.base_url + self.country_endpoint
         response = requests.get(countries_url, cookies={'user_cookie': self.cookie})
         countries = response.json()
         return countries
 
     def get_leaders(self, country):
+        '''Retrieves leader data for a specific country.'''
         leaders_url = f"{self.base_url}{self.leaders_endpoint}?country={country}"
         response = requests.get(leaders_url, cookies={'user_cookie': self.cookie})
         self.leaders_data[country] = response.json()
 
     def get_first_paragraph(wikipedia_url):
+        '''Retrieves the first paragraph of a Wikipedia page.''' 
 
         def find_p_with_b_start(tag):
+            '''Defines the strategy (first paragraph that start in bold text) for identifying first paragraph.'''
             return tag.name == 'p' and tag.b
         
         response = requests.get(wikipedia_url)
@@ -39,6 +44,7 @@ class WikipediaScraper:
         return first_paragraph
     
     def to_json_file(self, filepath):
+        '''Writes the leader data to a JSON file.'''
         with open(filepath, 'w') as file:
             json.dump(self.leaders_data, file, indent=4)
     
